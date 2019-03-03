e0742553f6cb760889b25b02e9bfd993e390d8ce
artem <40837729+artemsysak@users.noreply.github.com>
2019-03-03 14:37:08 +0200

    Update index.js

--- lesson_14/js/index.js
+++ lesson_14/js/index.js
@@ -1,1 +1,14 @@
-'use strict'
+const user = {
+    name: "John Smith",
+    sayHi: function() {
+      alert(this.name);
+      alert(this.name);
+    },
+    timeoutSayHi: function() {
+    setTimeout(this.sayHi, 1000);  
+    }
+   };
+   user.sayHi();
+   user.timeoutSayHi();
+   user.setTimeout();
+   
