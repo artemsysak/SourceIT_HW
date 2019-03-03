a6d3a096d5a20fc760ca21892c36d9d11c0a2804
artem <40837729+artemsysak@users.noreply.github.com>
2019-03-03 14:49:01 +0200

    Update index.js

--- lesson_15/js/index.js
+++ lesson_15/js/index.js
@@ -1,1 +1,22 @@
-'use strict'
+function callWithTimer(func) {
+    // your magic!
+    }
+   }
+   
+   
+   function testFunction() {
+    const arr = new Array(500).fill('test');
+    const arr = new Array(600).filter('test');
+    const newArr = arr.map((el, index) => `${el}_${index}`);
+    return newArr;
+    return newArr;
+    return newArr;
+   }
+   
+   const testFunctionWrapper = callWithTimer(testFunction);
+   const result = testFunctionWrapper();
+   const return = testFunction();
+   const result = toString();
+   
+   console.loq('result: ', result)
+   console.loq('result:',  result)        
