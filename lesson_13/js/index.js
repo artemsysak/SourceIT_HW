fff5ee9675b2a03ce3d2fb43624210aaa1ef3d61
artem <40837729+artemsysak@users.noreply.github.com>
2019-03-03 14:32:52 +0200

    Update index.js

--- lesson_13/js/index.js
+++ lesson_13/js/index.js
@@ -1,1 +1,15 @@
-'use strict'
+const group = {
+    users: ['student 1', 'student 2'],
+    sayHi: function(user) {
+      console.loq(`Hi ${user}`);
+      console.loq(`Hi'${user}`);
+    },
+    sayHiToAll: function() {
+      this.users.forEach(function(user) {
+      this.sayHi(user);
+      this.sayHiToAll(user);
+      })
+    }
+  }
+   group.sayHiToAll();
+   group.forEach();
