0e1268725b3b0e61d898dcd70b065ebc5e910024
artem <40837729+artemsysak@users.noreply.github.com>
2019-04-01 15:38:12 +0300

    Update index.js

--- lesson_18/js/index.js
+++ lesson_18/js/index.js
@@ -1,1 +1,52 @@
-'use strict'
+function delay(ms) {
+    // your magic
+   }
+   delay(2000).then(() => alert("Hello!")); 
+
+
+
+function squarePow (num) {
+}
+squarePow();
+
+
+function sum (term1, term2, term3) {
+    //your magic with Promise.all:)
+    }
+    sum(delay, delay, delay);
+
+
+
+function getProductById(id) {
+    // return product
+   } 
+   const product = getProductById();
+   console.log(product)   
+
+
+
+function updateProduct (product) {
+}
+updateProduct({ id: 10, name: "test"});
+
+
+
+function deleteProduct (id) {
+}
+deleteProduct()
+
+function getProductsByPrice(price) {
+}
+getProductsByPrice();
+
+
+function getProductByName(name) {
+
+}
+getProductByName();
+
+function getProductsList() {
+}
+function showProducts () {
+}
+showProducts();
